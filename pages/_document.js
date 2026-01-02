612e79d075b62182d86aeada66fc7257b439f2ce
dalemy-1 <9848014@gmail.com>
2026-01-02 14:25:36 +0800

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* 引入 Font Awesome 图标库 */}
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
