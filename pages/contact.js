b60d3895bbe024f586b38aea9e2e75024fdab070
dalemy-1 <9848014@gmail.com>
2026-01-02 13:31:53 +0800

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,18 @@
+// pages/contact.js
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form>
+        <label>Name:</label>
+        <input type="text" />
+        <br />
+        <label>Email:</label>
+        <input type="email" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
