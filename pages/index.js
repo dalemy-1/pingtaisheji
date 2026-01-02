2d54e1a6c92a50a5406dc135a596fcac52ed46c6
dalemy-1 <9848014@gmail.com>
2026-01-02 13:32:05 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 // pages/index.js
+import Link from 'next/link'; // 引入 Link 组件
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to My Platform!</h1>
-      <p>Here is where the magic happens.</p>
+      <nav>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
