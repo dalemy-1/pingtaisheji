191cfd7bad016ea5e2b821bf434d2b5d8fe9971e
dalemy-1 <9848014@gmail.com>
2026-01-02 13:31:42 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,10 @@
 // pages/about.js
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page where you can learn more about our platform.</p>
+    </div>
+  );
 }
